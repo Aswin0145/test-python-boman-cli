513cc2377f4c44bd14c511f8d933a8fc11af1911
Aswin0145 <aswin01456@gmail.com>
2023-03-28 07:14:04 -0400

    mc

--- test-jenkins.py
+++ test-jenkins.py
@@ -10,9 +10,9 @@ file_name = 'boman_baseline.json'
 uid = os.getuid()
 gid = os.getgid()
 userid= f"{uid}:{gid}"
-cmd2 = "docker run -v $(pwd):{bind}:rw --user {userid} -t owasp/zap2docker-stable zap-baseline.py -t {url} -g gen.conf -J {file}"
+cmd2 = "docker run -v $(pwd):{bind}:rw --user --user $(id -u jenkins):$(id -g jenkins) -t owasp/zap2docker-stable zap-baseline.py -t {url} -g gen.conf -J {file}"
 
-final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name, user_id =userid)
+final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name)
 
 
 ar = str(final_command)
