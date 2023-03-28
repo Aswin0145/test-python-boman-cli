e49a43004a89b85b23242634cf9e2cc86ac815ca
Aswin0145 <aswin01456@gmail.com>
2023-03-28 04:49:31 -0400

    update py

--- test-jenkins.py
+++ test-jenkins.py
@@ -8,9 +8,9 @@ url = 'https://demo.testfire.net/'
 file_name = 'boman_baseline.json'
 
 
-cmd2 = "docker run -v $(pwd):{bind}:rw -t owasp/zap2docker-stable zap-baseline.py -t {url} -g gen.conf -J {file}"
+cmd2 = "docker run -v $(pwd):{bind}:rw --user {userid} -t owasp/zap2docker-stable zap-baseline.py -t {url} -g gen.conf -J {file}"
 
-final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name)
+final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name, user_id =user_id)
 
 
 ar = str(final_command)
