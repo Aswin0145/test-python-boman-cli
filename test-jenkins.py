575b29f0407a315d51838b18c696a187f481a8ac
Aswin0145 <aswin01456@gmail.com>
2023-03-28 04:51:43 -0400

    updated user id

--- test-jenkins.py
+++ test-jenkins.py
@@ -7,17 +7,16 @@ bind_folder = '/zap/wrk/'
 url = 'https://demo.testfire.net/'
 file_name = 'boman_baseline.json'
 
-
+uid = os.getuid()
+gid = os.getgid()
+userid= f"{uid}:{gid}"
 cmd2 = "docker run -v $(pwd):{bind}:rw --user {userid} -t owasp/zap2docker-stable zap-baseline.py -t {url} -g gen.conf -J {file}"
 
-final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name, user_id =user_id)
+final_command = "% s" % cmd2.format(url = url, bind = bind_folder, file = file_name, user_id =userid)
 
 
 ar = str(final_command)
 
 ##print(final_command);
-uid = os.getuid()
-gid = os.getgid()
-userid= f"{uid}:{gid}"
 print(userid)
 os.system(ar)
